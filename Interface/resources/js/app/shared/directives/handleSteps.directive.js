385e5e898f312eac6817c6747790905d33a4db10
Sune Radich Vestergaard | 1508 <srv@1508.dk>
2016-01-12 13:52:26 +0100

    Switch between steps when tabbing out of last input field, or back from first input field on a step
    Note: Current implementation does not work when going back in Chrome, but works in Safari for mac and IE10

--- Interface/resources/js/app/shared/directives/handleSteps.directive.js
+++ Interface/resources/js/app/shared/directives/handleSteps.directive.js
@@ -20,7 +20,8 @@ define([
 
                     var target = $(event.target),
                         isFirst = target.closest('.first').length > 0,
-                        isLast = target.closest('.last').length > 0;
+                        isLast = target.closest('.last').length > 0,
+                        time;
 
                     //Tab
                     if (event.keyCode === 9) {
@@ -28,11 +29,24 @@ define([
                         if (event.shiftKey && isFirst) {
                             
                             scope.prevFunc();
+
+                            //Need a timeout to let the routing system change view
+                            time = $timeout(function() {
+                                $(element).find('input:visible:last').focus();
+                            }, 300);
                             
-                            $timeout(function() {
-                                //TODO why does this not work?!
-                                $(element).find('input:last').focus();
-                            }, 0);
+
+                            // scope.$apply(function() {
+                            //     console.log('focusing', $(element).find('input:last'));
+                            //     $(element).find('input:last').focus();
+                            // });
+                            
+                            //timeout required otherwise the change does not kick in until next digest??
+                            // $timeout(function() {
+                            //     //TODO why does this not work?!
+                            //     //This does not set focus
+                            //     $(element).find('input:last').focus();
+                            // }, 0);
                         }
 
                         else if (!event.shiftKey && isLast) {
@@ -42,6 +56,8 @@ define([
                                 $(element).find('input:first').focus();
                             }, 0);
                         }
+
+                        return true;
                     }
                 });
 
@@ -49,6 +65,7 @@ define([
                 //Cleanup
                 scope.$on('$destroy', function() {
                     $(element).off('keydown');
+                    time.cancel();
                 });
 
             }
