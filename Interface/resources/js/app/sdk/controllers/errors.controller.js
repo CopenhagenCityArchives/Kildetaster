66f9e0eef869acd336cebf40a92ab72e67dbc48d
Sune Radich Vestergaard | 1508 <srv@1508.dk>
2016-03-03 11:29:26 +0100

    Updated to use correct api

--- Interface/resources/js/app/sdk/controllers/errors.controller.js
+++ Interface/resources/js/app/sdk/controllers/errors.controller.js
@@ -2,17 +2,36 @@ define([
 
 ], function() {
 
-    var errorsController = /*@ngInject*/ function mypageController($scope, errorService, EDITORURL) {
+    var errorsController = /*@ngInject*/ function mypageController($scope, tokenService, errorService, EDITORURL) {
 
-        $scope.loading = true;
+        $scope.loading = false;
         $scope.errorList = [];
-       
-        errorService.getData().then(function(response) {
-            $scope.errorList = response;
-        }).finally(function() {
-            $scope.loading = false;
+
+        $scope.userId = null;
+
+        tokenService.requestToken().then(function(response) {
+            var data = response.tokenData;
+            $scope.userId = data.user_id;
+            //Initialize logic
+            $scope.init();
         });
 
+
+        $scope.init = function() {
+
+            $scope.loading = true;
+
+            //Get error reports for a given user
+            errorService.getErrorReports({
+                relevant_user_id: $scope.userId,
+                //TODO: Remove hardcoded taskid for beta
+                task_id: 1
+            }).then(function(response) {
+                $scope.errorList = response;
+            }).finally(function() {
+                $scope.loading = false;
+            });
+        };
 
     };
 
