a9d0388b1608d4d32cb1baf0e32ae76e74c6f0c0
Sune Radich Vestergaard | 1508 <srv@1508.dk>
2016-03-03 10:50:51 +0100

    Added token service and factory

---
+++ Interface/resources/js/app/shared/services/token.factory.js
@@ -0,0 +1,50 @@
+define([
+
+], function() {
+
+    //Create a http interceptor factory
+    var accessTokenHttpInterceptor = /*@ngInject*/ function accessTokenHttpInterceptor(MAINDOMAIN, $sessionStorage) {
+
+        return {
+
+            //For each request the interceptor will set the bearer token header.
+            request: function($config) {
+
+
+                if ($config.url !== MAINDOMAIN + '/index.php') {
+
+                    if ($sessionStorage.tokenData) {
+                        //Fetch token from cookie
+                        var token = $sessionStorage.tokenData.access_token;
+
+                        //set authorization header
+                        $config.headers['Authorization'] = 'Bearer ' + token;
+                    }
+
+                }
+
+                return $config;
+
+            },
+
+            response: function(response) {
+                //if you get a token back in your response you can use 
+                //the response interceptor to update the token in the 
+                //stored in the cookie
+
+                // if (response.config.headers.yourTokenProperty) {
+                //       //fetch token
+                //       var token = response.config.headers.yourTokenProperty;
+
+                //       //set token
+                //       $cookies.put('token', token);
+                // }
+
+                return response;
+            }
+        };
+    };
+
+    return accessTokenHttpInterceptor;
+
+});
---
+++ Interface/resources/js/app/shared/services/token.service.js
@@ -0,0 +1,83 @@
+define([
+
+
+], function() {
+
+    var tokenService = /*@ngInject*/ function tokenService($q, MAINDOMAIN, $http, $sessionStorage) {
+
+        console.log('run')
+
+        return {
+
+            requestToken: function() {
+
+                var deferred = $q.defer();
+                var formData = new FormData();
+
+                formData.append('authorized', 1);
+                formData.append('state', 'kildetaster');
+
+                $http({
+                        method: 'POST',
+
+                        url: MAINDOMAIN + '/index.php',
+                        headers: {
+                            'Content-Type': undefined //'application/x-www-form-urlencoded'
+                        },
+                        transformRequest: angular.identity,
+                        // transformRequest: function(obj) {
+                        //     var str = [];
+                        //     for (var p in obj)
+                        //         str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                        //     return str.join("&");
+                        // },
+                        params: {
+                            option: 'authorize',
+                            response_type: 'token',
+                            client_id: 'kbhkilder',
+                            api: 'oauth2'
+                        },
+                        data: formData
+                    })
+                    .then(function(response) {
+
+                        //We got data back from the request, we are loggeld in and can save to sessionStorage
+                        if (typeof response.data === 'object') {
+                            //console.log('tokenData', response.data);
+                            $sessionStorage.tokenData = response.data;
+
+                            deferred.resolve({ tokenData: response.data });
+                        }
+                        //We are not logged in, point users to min-side
+                        else {
+                            window.location.href = MAINDOMAIN + '/min-side';
+                        }
+
+                    })
+                    .catch(function(err) {
+                        console.log('err', err);
+                    });
+
+                return deferred.promise;
+
+            },
+
+
+            getToken: function() {
+                //console.log('data', $sessionStorage.tokenData);
+                if ($sessionStorage.tokenData) {
+                    return $sessionStorage.tokenData.access_token;
+                }
+
+                return null;
+            }
+
+
+
+        };
+
+    };
+
+    return tokenService;
+
+});
