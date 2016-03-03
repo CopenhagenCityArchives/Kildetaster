740a25358b5ff97a22775e237b13fa5392285c9f
Sune Radich Vestergaard | 1508 <srv@1508.dk>
2016-03-03 10:48:14 +0100

    Added constant for MAINDOMAIN and TOKENURL

--- Interface/grunttasks/ngconstant.js
+++ Interface/grunttasks/ngconstant.js
@@ -10,7 +10,9 @@ module.exports = {
             JSONURL: '/resources/mock/',
             EDITORURL: 'http://localhost:1508',
             SDKCSSURL: '/resources/css/sdk.css',
-            API: 'http://kbhkilder.dk/1508/stable'
+            API: 'http://kbhkilder.dk/1508/stable',
+            MAINDOMAIN: 'http://kbharkiv.bo.intern.redweb.dk',
+            TOKENURL: 'http://kbharkiv.bo.intern.redweb.dk/index.php?option=authorize&response_type=token&client_id=kbhkilder&api=oauth2'
         }
     },
 
@@ -19,7 +21,9 @@ module.exports = {
             JSONURL: '/resources/mock/',
             EDITORURL: 'http://kbharkiv.dk/kildetaster',
             SDKCSSURL: 'http://localhost:1508/resources/css/sdk.css',
-            API: 'http://kbhkilder.dk/1508/stable'
+            API: 'http://kbhkilder.dk/1508/stable',
+            MAINDOMAIN: 'http://kbharkiv.bo.intern.redweb.dk',
+            TOKENURL: 'http://kbharkiv.bo.intern.redweb.dk/index.php?option=authorize&response_type=token&client_id=kbhkilder&api=oauth2'
         }
     },
 
@@ -28,7 +32,9 @@ module.exports = {
             JSONURL: 'http://kildetaster.dev9.1508test.dk/resources/mock/',
             EDITORURL: 'http://kbharkiv.dk/kildetaster',
             SDKCSSURL: 'http://kbhkilder.dk/software/kildetaster/resources/css/sdk.css',
-            API: 'http://kbhkilder.dk/1508/stable'
+            API: 'http://kbhkilder.dk/1508/stable',
+            MAINDOMAIN: 'http://kbharkiv.bo.intern.redweb.dk',
+            TOKENURL: 'http://kbharkiv.bo.intern.redweb.dk/index.php?option=authorize&response_type=token&client_id=kbhkilder&api=oauth2'
         }
     }
 };
