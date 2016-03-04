086311c4c7b3208da7c2dff7c0983eeb79cc79b7
Sune Radich Vestergaard | 1508 <srv@1508.dk>
2016-03-04 10:29:58 +0100

    Removed temp 8080 urls

--- Interface/grunttasks/ngconstant.js
+++ Interface/grunttasks/ngconstant.js
@@ -12,7 +12,9 @@ module.exports = {
             SDKCSSURL: '/resources/css/sdk.css',
             API: 'http://kbhkilder.dk/1508/stable',
             MAINDOMAIN: 'http://kbharkiv.bo.intern.redweb.dk',
-            TOKENURL: 'http://kbharkiv.bo.intern.redweb.dk/index.php?option=authorize&response_type=token&client_id=kbhkilder&api=oauth2'
+            TOKENURL: 'http://kbharkiv.bo.intern.redweb.dk/index.php?option=authorize&response_type=token&client_id=kbhkilder&api=oauth2',
+            ERRORURL: 'http://kbharkiv.dk/deltag/indtast-data-fra-arkivmateriale',
+            SEARCHURL: 'http://kbharkiv.bo.intern.redweb.dk/sog-i-arkivet/sog-efter-personer'
         }
     },
 
@@ -23,18 +25,22 @@ module.exports = {
             SDKCSSURL: 'http://localhost:1508/resources/css/sdk.css',
             API: 'http://kbhkilder.dk/1508/stable',
             MAINDOMAIN: 'http://kbharkiv.bo.intern.redweb.dk',
-            TOKENURL: 'http://kbharkiv.bo.intern.redweb.dk/index.php?option=authorize&response_type=token&client_id=kbhkilder&api=oauth2'
+            TOKENURL: 'http://kbharkiv.bo.intern.redweb.dk/index.php?option=authorize&response_type=token&client_id=kbhkilder&api=oauth2',           
+            ERRORURL: 'http://kbharkiv.dk/deltag/indtast-data-fra-arkivmateriale',
+            SEARCHURL: 'http://kbharkiv.bo.intern.redweb.dk/sog-i-arkivet/sog-efter-personer'
         }
     },
 
     production: {
         constants: {
             JSONURL: 'http://kildetaster.dev9.1508test.dk/resources/mock/',
-            EDITORURL: 'http://kbharkiv.dk/kildetaster',
+            EDITORURL: '/kildetaster',
             SDKCSSURL: 'http://kbhkilder.dk/software/kildetaster/resources/css/sdk.css',
             API: 'http://kbhkilder.dk/1508/stable',
             MAINDOMAIN: 'http://kbharkiv.bo.intern.redweb.dk',
-            TOKENURL: 'http://kbharkiv.bo.intern.redweb.dk/index.php?option=authorize&response_type=token&client_id=kbhkilder&api=oauth2'
+            TOKENURL: 'http://kbharkiv.bo.intern.redweb.dk/index.php?option=authorize&response_type=token&client_id=kbhkilder&api=oauth2',
+            ERRORURL: 'http://kbharkiv.dk/deltag/indtast-data-fra-arkivmateriale',
+            SEARCHURL: 'http://kbharkiv.bo.intern.redweb.dk/sog-i-arkivet/sog-efter-personer'
         }
     }
 };
