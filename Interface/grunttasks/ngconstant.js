04fb29aa318e14b30364784ea5ee493ef630ab76
Sune Radich Vestergaard | 1508 <srv@1508.dk>
2016-03-14 13:20:49 +0100

    Updated path constants to point to real endpoints

--- Interface/grunttasks/ngconstant.js
+++ Interface/grunttasks/ngconstant.js
@@ -11,11 +11,11 @@ module.exports = {
             EDITORURL: 'http://localhost:1508',
             RESSOURCEURL: '/resources/',
             SDKCSSURL: '/resources/css/sdk.css',
-            API: 'http://kbhkilder.dk/1508/stable',
-            MAINDOMAIN: 'http://kbharkiv.bo.intern.redweb.dk',
-            TOKENURL: 'http://kbharkiv.bo.intern.redweb.dk/index.php?option=authorize&response_type=token&client_id=kbhkilder&api=oauth2',
-            ERRORURL: 'http://kbharkiv.dk/deltag/indtast-data-fra-arkivmateriale',
-            SEARCHURL: 'http://kbharkiv.bo.intern.redweb.dk/sog-i-arkivet/sog-efter-personer'
+            API: 'http://www.kbhkilder.dk/1508/stable',
+            MAINDOMAIN: 'http://www.kbharkiv.dk',
+            TOKENURL: 'http://www.kbharkiv.dk/index.php?option=authorize&response_type=token&client_id=kbhkilder&api=oauth2',
+            ERRORURL: 'http://www.kbharkiv.dk/deltag/indtast-data-fra-arkivmateriale',
+            SEARCHURL: 'http://www.kbharkiv.dk/sog-i-arkivet/sog-efter-personer'
         }
     },
 
@@ -25,11 +25,11 @@ module.exports = {
             EDITORURL: 'http://kbharkiv.dk/kildetaster',
             RESSOURCEURL: 'http://localhost:1508/resources/',
             SDKCSSURL: 'http://localhost:1508/resources/css/sdk.css',
-            API: 'http://kbhkilder.dk/1508/stable',
-            MAINDOMAIN: 'http://kbharkiv.bo.intern.redweb.dk',
-            TOKENURL: 'http://kbharkiv.bo.intern.redweb.dk/index.php?option=authorize&response_type=token&client_id=kbhkilder&api=oauth2',           
-            ERRORURL: 'http://kbharkiv.dk/deltag/indtast-data-fra-arkivmateriale',
-            SEARCHURL: 'http://kbharkiv.bo.intern.redweb.dk/sog-i-arkivet/sog-efter-personer'
+            API: 'http://www.kbhkilder.dk/1508/stable',
+            MAINDOMAIN: 'http://www.kbharkiv.dk',
+            TOKENURL: 'http://www.kbharkiv.dk/index.php?option=authorize&response_type=token&client_id=kbhkilder&api=oauth2',           
+            ERRORURL: 'http://www.kbharkiv.dk/deltag/indtast-data-fra-arkivmateriale',
+            SEARCHURL: 'http://www.kbharkiv.dk/sog-i-arkivet/sog-efter-personer'
         }
     },
 
@@ -37,13 +37,13 @@ module.exports = {
         constants: {
             JSONURL: 'http://kildetaster.dev9.1508test.dk/resources/mock/',
             EDITORURL: '/kildetaster',
-            RESSOURCEURL: 'http://kbhkilder.dk/software/kildetaster/resources/',
-            SDKCSSURL: 'http://kbhkilder.dk/software/kildetaster/resources/css/sdk.css',
-            API: 'http://kbhkilder.dk/1508/stable',
-            MAINDOMAIN: 'http://kbharkiv.bo.intern.redweb.dk',
-            TOKENURL: 'http://kbharkiv.bo.intern.redweb.dk/index.php?option=authorize&response_type=token&client_id=kbhkilder&api=oauth2',
-            ERRORURL: 'http://kbharkiv.dk/deltag/indtast-data-fra-arkivmateriale',
-            SEARCHURL: 'http://kbharkiv.bo.intern.redweb.dk/sog-i-arkivet/sog-efter-personer'
+            RESSOURCEURL: 'http://www.kbhkilder.dk/software/kildetaster/resources/',
+            SDKCSSURL: 'http://www.kbhkilder.dk/software/kildetaster/resources/css/sdk.css',
+            API: 'http://www.kbhkilder.dk/1508/stable',
+            MAINDOMAIN: 'http://www.kbharkiv.dk',
+            TOKENURL: 'http://www.kbharkiv.dk/index.php?option=authorize&response_type=token&client_id=kbhkilder&api=oauth2',
+            ERRORURL: 'http://www.kbharkiv.dk/deltag/indtast-data-fra-arkivmateriale',
+            SEARCHURL: 'http://www.kbharkiv.dk/sog-i-arkivet/sog-efter-personer'
         }
     }
 };
