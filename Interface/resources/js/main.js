92f2e29bd79fa161f31e05f54d06e9929bfeb68e
Sune Radich Vestergaard | 1508 <srv@1508.dk>
2016-03-15 15:49:54 +0100

    Clean up and comments added

--- Interface/resources/js/main.js
+++ Interface/resources/js/main.js
@@ -6,7 +6,7 @@ requirejs([
 
     'app/editor/editor'
 
-], function ($, editorApp, apiApp) {
+], function ($, editorApp) {
     
     'use strict';
 
@@ -26,6 +26,9 @@ requirejs([
         //
     };
 
+
+//http://stackoverflow.com/questions/34545875/how-to-store-authentication-bearer-token-in-browser-cookie-using-angularjs
+//http://stackoverflow.com/questions/22537311/angular-ui-router-login-authentication
 
     /* Attach events
     ----------------------------*/
