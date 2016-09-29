4589fe5fd78358b04b56d8e1e441de16a29a25b1
Sune Radich Vestergaard | 1508 <srv@1508.dk>
2016-09-29 11:43:47 +0200

    Test against integer value
    The api was updated to return numbers as integers instead of strings. So we need to compare against 1, and not '1'
    
    https://trello.com/c/45Mb1XRN/59-tekst-vises-ikke-pa-listen-over-s-geresultatet

--- Interface/resources/js/app/sdk/directives/searchresult.directive.js
+++ Interface/resources/js/app/sdk/directives/searchresult.directive.js
@@ -6,7 +6,7 @@ define([
     var searchDirective = /*@ngInject*/ function(API, $state, helpers, searchService) {
 
         return {
-            
+
             restrict: 'E',
             replace: true,
 
@@ -28,21 +28,21 @@ define([
                         postId: scope.result.post_id
                     });
                 };
-               
+
                 scope.lookItUp = function(str) {
 
                     var found = scope.config.find(function(itm) {
                        //Return the item that matches the solr_name and is marked as include in result
-                       return str === itm.solr_name && itm.include_in_result === '1';
+                       return str === itm.solr_name && itm.include_in_result === 1;
                     });
 
                     if (found !== undefined) {
-                        return found.name;    
+                        return found.name;
                     }
                     else {
                         return null;
                     }
-                    
+
                 };
 
                 scope.buildValue = function(val) {
