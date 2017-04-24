35cedba3098267e3e5d8d863e01173718663285d
Bo Henriksen <stumpdenmark@hotmail.com>
2017-04-24 15:33:37 +0200

    Changed deploy settings

--- Interface/grunttasks/ftp-deploy.js
+++ Interface/grunttasks/ftp-deploy.js
@@ -3,39 +3,39 @@ module.exports = {
     auth: {
       host: 'kbhkilder.dk',
       port: 21,
-      authKey: 'kbharkiv'
+      authKey: 'kbhkilder'
     },
     forceVerbose: true,
     src: '<%= package.build %>',
     dest: 'public_html/software/kildetaster-development',
     exclusions : [ '<%= package.build %>' + '/resources']
   },
-  deployment_index_file: {
-    auth: {
-      host: 'kbharkiv.dk',
-      port: 21,
-      authKey: 'kbhkilder'
-    },
-    forceVerbose : true,
-    src: '<%= package.build %>',
-    dest: 'public_html/kildetaster-development'
-  },
   production_resources: {
     auth: {
       host: 'kbhkilder.dk',
       port: 21,
-      authKey: 'kbharkiv'
+      authKey: 'kbhkilder'
     },
     forceVerbose: true,
     src: '<%= package.build %>',
     dest: 'public_html/software/kildetaster',
     exclusions : [ '<%= package.build %>' + '/resources/**']
   },
+  deployment_index_file: {
+    auth: {
+      host: 'kbharkiv.dk',
+      port: 21,
+      authKey: 'kbharkiv'
+    },
+    forceVerbose : true,
+    src: '<%= package.build %>',
+    dest: 'public_html/kildetaster-development'
+  },
   production_index_file: {
     auth: {
       host: 'kbharkiv.dk',
       port: 21,
-      authKey: 'kbhkilder'
+      authKey: 'kbharkiv'
     },
     forceVerbose : true,
     src: '<%= package.build %>',
