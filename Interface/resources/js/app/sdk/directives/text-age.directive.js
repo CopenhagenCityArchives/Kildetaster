24f3acd17c730589e92d0d9034b27b6c7cb91a3b
Sune Radich Vestergaard | 1508 <srv@1508.dk>
2017-06-20 09:34:24 +0200

    Work in progress on new fields in post view

--- Interface/resources/js/app/sdk/directives/text-age.directive.js
+++ Interface/resources/js/app/sdk/directives/text-age.directive.js
@@ -11,11 +11,24 @@ define([
 
             scope: {
                 years: '=',
-                month: '='
+                months: '=',
+                weeks: '=',
+                days: '=',
+                hours: '='
             },
 
             link: function(scope, element, attr) {
-                //noop
+
+                console.log(scope);
+
+                //alle kan have decimaler
+                scope.days = scope.days;
+                scope.daysText = scope.days === '1,00' ? 'dag' : 'dage';
+
+                scope.weeks = scope.weeks;
+
+                scope.hours = scope.hours;
+
             }
         };
 
