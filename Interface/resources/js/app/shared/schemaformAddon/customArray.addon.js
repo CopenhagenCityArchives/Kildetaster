b0b873ad158961d6c461eaef23f4c569d570bbb7
Sune Radich Vestergaard | 1508 <srv@1508.dk>
2017-08-21 15:09:35 +0200

    Focus first typeahead search field in array form structures
    https://trello.com/c/aJSVM07S/154-indtastning-klik-tilf%C3%B8j-skal-aktivere-skrivefeltet-erhverv-og-d%C3%B8ds%C3%A5rsag

--- Interface/resources/js/app/shared/schemaformAddon/customArray.addon.js
+++ Interface/resources/js/app/shared/schemaformAddon/customArray.addon.js
@@ -8,6 +8,63 @@ define([
 
     var schemaForm = angular.module('schemaForm');
 
+    schemaForm.controller('sfCustomArray', /*@ngInject*/  function($scope, $element, $timeout) {
+
+        /**
+        * Add item to array, proxy function to do custom logic when calling
+        * schemaForm default add functionality
+        *
+        * @param callback {function} The default schemaform add function
+        * @param event {event} An angular click event object
+        */
+        $scope.add = function add(callback, event) {
+
+            //Timeout to match the default animation duration for ng-enter, @see _list-group-item.scss
+            var timeout = 210;
+
+            if (callback && angular.isFunction(callback)) {
+
+                //Call the default click function
+                callback();
+
+                //Give angular time to add the new item and make it available in the dom
+                $timeout(function() {
+
+                    var selectController, elm,
+
+                        //locate the list-group items in the dom node we are working on
+                        listElements = $(event.currentTarget).closest('.schema-form-array').find('.list-group-item');
+
+                    //Get the last element if we can
+                    if (listElements.length > 0) {
+
+                        elm = listElements[listElements.length -1];
+
+                        //get a reference to the custom typeahead
+                        selectController = $(elm).find('sf-decorator:first .ui-select-match').controller('uiSelect');
+
+                        //Force the typeahead to open, and focus the search input in it
+                        $timeout(function() {
+
+                            selectController.activate(false, true);
+
+                            //Give the directive time to activate
+                            $scope.$apply();
+
+                            //Focus the input
+                            selectController.focusSearchInput();
+                        }, timeout);
+                    }
+
+                });
+
+
+
+            }
+
+        }
+    });
+
     schemaForm.config(function(schemaFormDecoratorsProvider, sfBuilderProvider) {
 
         //Define a custom decorator for array type constructs. We need to be able to change the html
