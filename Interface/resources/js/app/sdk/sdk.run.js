1500559596ffa40f62847292eaa8483a7c1ca18a
Sune Radich Vestergaard | 1508 <srv@1508.dk>
2017-10-30 14:44:58 +0100

    Suppress statechanges errors if a  change is issued before another finishes
    @see https://ui-router.github.io/guide/ng1/migrate-to-1_0

--- Interface/resources/js/app/sdk/sdk.run.js
+++ Interface/resources/js/app/sdk/sdk.run.js
@@ -2,7 +2,10 @@ define([
 
 ], function() {
 
-    var sdkRun = /*@ngInject*/ function sdkRun(SDKCSSURL) {
+    var sdkRun = /*@ngInject*/ function sdkRun(SDKCSSURL, $state, $trace) {
+
+        // Enable debugging of state to the console
+        // $trace.enable('TRANSITON');
 
         //Load the css file for the SDK
         var fileref = document.createElement("link");
@@ -10,6 +13,14 @@ define([
         fileref.type = "text/css";
         fileref.href = SDKCSSURL;
         document.getElementsByTagName("head")[0].appendChild(fileref);
+
+        // Hack to suppress new change changes before old finishes
+        // @see https://ui-router.github.io/guide/ng1/migrate-to-1_0
+        window.myAppErrorLog = [];
+        $state.defaultErrorHandler(function (error) {
+            // This is a naive example of how to silence the default error handler.
+            window.myAppErrorLog.push(error);
+        });
 
     };
 
