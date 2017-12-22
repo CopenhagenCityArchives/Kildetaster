8fb5722df5ba221f1df17cbee8fc87dcadf9de46
jfeo <jens@feodor.dk>
2017-12-22 13:24:30 +0100

    Add Asset Proxy hyperlinks to erindringer

--- Interface/resources/js/app/sdk/search/erindring/erindring.controller.js
+++ Interface/resources/js/app/sdk/search/erindring/erindring.controller.js
@@ -1,9 +1,9 @@
 define([], function() {
 
-    var erindringController = /*@ngInject*/ function erindringController(helpers, EDITORURL) {
+    var erindringController = /*@ngInject*/ function erindringController(helpers, API, EDITORURL) {
 
         var that = this;
-        
+
         this.$onInit = function() {
             that.imageUrl = helpers.getImageUrlByPostId(this.data.post_id);
 
@@ -11,8 +11,18 @@ define([], function() {
             that.showEditorLink = that.data.user_can_edit;
 
             that.editorUrl = EDITORURL + '/#/task/' + that.data.task_id + '/page/' + that.data.page_id + '/post/' + that.data.post_id;
-            
+
         };
+
+        this.getAssetHref = function() {
+            var id = that.data.id.split("-")[1];
+            return API + "/asset/" + id;
+        }
+
+        this.getTranscribedAssetHref = function() {
+            var id = that.data.transcribed_id;
+            return API + "/asset/" + id;
+        }
 
     };
 
