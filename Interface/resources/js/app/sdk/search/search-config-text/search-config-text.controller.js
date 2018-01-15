d21e6ac923a82f2f4c3e7b34469cffbfded3e6fd
Bo Henriksen <stumpdenmark@hotmail.com>
2018-01-15 13:24:08 +0100

    Dates are converted when displayed in search config

--- Interface/resources/js/app/sdk/search/search-config-text/search-config-text.controller.js
+++ Interface/resources/js/app/sdk/search/search-config-text/search-config-text.controller.js
@@ -1,5 +1,5 @@
 define([], function () {
-    var searchConfigTextController = /*@ngInject*/ function searchConfigTextController() {
+    var searchConfigTextController = /*@ngInject*/ function searchConfigTextController($filter) {
 
         var that = this;
 
@@ -12,6 +12,12 @@ define([], function () {
             angular.forEach(this.collections, function(val, key) {
                 if (val.selected) {
                     that.selectedCollections.push(val);
+                }
+            });
+
+            angular.forEach(this.config, function(val, key){
+                if(val.field.type == "date"){
+                    val.converted_term = $filter('date')(val.term, 'dd-MM-yyyy');
                 }
             });
         }
