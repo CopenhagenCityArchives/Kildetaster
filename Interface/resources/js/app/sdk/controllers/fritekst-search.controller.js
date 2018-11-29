5a9440901616b216dbfd103cf83a0eb4d8407c8f
Bo Henriksen <stumpdenmark@hotmail.com>
2018-11-29 13:32:53 +0100

    Free text search from frontpage now searches in all collections

--- Interface/resources/js/app/sdk/controllers/fritekst-search.controller.js
+++ Interface/resources/js/app/sdk/controllers/fritekst-search.controller.js
@@ -9,7 +9,7 @@ define([
             var term = encodeURIComponent($scope.term);
             // Go to the search page and perform the search
 
-            document.location.href = $scope.searchUrl + '#/results?q1.f=freetext_store&q1.op=in_multivalued&q1.t=' + term + '&sortField=lastname&sortDirection=asc&postsPrPage=10&collections=1,17,18';
+            document.location.href = $scope.searchUrl + '#/results?q1.f=freetext_store&q1.op=in_multivalued&q1.t=' + term + '&sortField=lastname&sortDirection=asc&postsPrPage=10';
         }
 
         $scope.init = function init(options) {
