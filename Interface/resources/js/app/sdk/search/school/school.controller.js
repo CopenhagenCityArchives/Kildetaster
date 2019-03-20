fe15cd9a5faca967eb0a1c9c9192498526d0c95b
Jens Feodor Nielsen <jensfeodor@gmail.com>
2019-03-20 11:40:00 +0100

    Add permalink to school protocol post view.

--- Interface/resources/js/app/sdk/search/school/school.controller.js
+++ Interface/resources/js/app/sdk/search/school/school.controller.js
@@ -1,10 +1,12 @@
-define([], function() {
+define([], function () {
 
     var schoolController = /*@ngInject*/ function schoolController(helpers, $scope) {
 
         var that = this;
 
         $scope.isNumber = angular.isNumber;
+
+        that.permalink = "https://kbharkiv.dk/permalink/post/" + that.data.id;
 
     };
 
