d31b136793de78947287561ddee50a920e09c689
Bo Henriksen <stumpdenmark@hotmail.com>
2019-05-29 10:08:40 +0200

    whatever

--- Interface/resources/js/app/sdk/controllers/useractivities.controller.js
+++ Interface/resources/js/app/sdk/controllers/useractivities.controller.js
@@ -1,45 +1,36 @@
 define([
 
-], function () {
+], function() {
 
-    var useractivitiesController = /*@ngInject*/ function mypageController($scope, tokenService, EDITORURL, userService, taskService) {
+    var useractivitiesController = /*@ngInject*/ function mypageController($scope, tokenService, EDITORURL, userService) {
 
         $scope.loading = false;
         $scope.activities = [];
-        $scope.tasks = {};
 
         $scope.userId = null;
 
-        tokenService.requestToken().then(function (response) {
+        tokenService.requestToken().then(function(response) {
             var data = response.tokenData;
             $scope.userId = data.user_id;
             //Initialize logic
             $scope.init();
         });
 
-        $scope.EDITORURL = EDITORURL;
+        $scope.EDITORURL = EDITORURL;        
 
-        $scope.init = function () {
+        $scope.init = function() {
 
             $scope.loading = true;
 
+            //Get error reports for a given user
+            userService.getUserActivities({
+                user_id: $scope.userId
+            }).then(function(response) {
+                $scope.activities = response;
 
-
-            taskService.getTasks().then(function (response) {
-                response.forEach(function (task) {
-                    $scope.tasks[task.id] = task;
-                });
-                userService.getUserActivities({
-                    user_id: $scope.userId
-                }).then(function (response) {
-                    $scope.activities = response.filter(function (activity) {
-                        return activity.task_unit_pages_done < activity.unit_pages;
-                    });
-                }).finally(function () {
-                    $scope.loading = false;
-                });
+            }).finally(function() {
+                $scope.loading = false;
             });
-
         };
 
     };
