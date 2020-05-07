d5c5d17b958232fd06ae68a962b5d6ddd650690a
jfeo <jens@feodor.dk>
2020-05-07 11:54:51 +0200

    Update sdk.run.js Do not add CSS to head.

--- Interface/resources/js/app/sdk/sdk.run.js
+++ Interface/resources/js/app/sdk/sdk.run.js
@@ -37,13 +37,6 @@ define([
         // https://ui-router.github.io/ng1/docs/1.0.0/enums/trace.category.html
         //$trace.enable('TRANSITION');
 
-        //Load the css file for the SDK
-        var fileref = document.createElement("link");
-        fileref.rel = "stylesheet";
-        fileref.type = "text/css";
-        fileref.href = SDKCSSURL;
-        document.getElementsByTagName("head")[0].appendChild(fileref);
-
         // Hack to suppress new change changes before old finishes
         // @see https://ui-router.github.io/guide/ng1/migrate-to-1_0
         window.myAppErrorLog = [];
