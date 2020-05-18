6e3eaee9f830090164b039a19d6f035158445f6c
jfeo <jens@feodor.dk>
2020-05-18 18:54:55 +0200

    Update angular-auth0.js to reflect current version of auth0

--- Interface/resources/js/libs/angular-auth0.js
+++ Interface/resources/js/libs/angular-auth0.js
@@ -9,52 +9,78 @@ function(Auth0, angular) {
     .module('auth0.auth0', [])
     .provider('angularAuth0', angularAuth0);
 
-    function angularAuth0() {
-    if (!angular.isFunction(Auth0)) {
-      throw new Error('Auth0 must be loaded.');
-    }
+  if (typeof angular !== 'object') {
+    throw new Error('Angular must be loaded.');
+  }
 
+  if (!angular.isObject(Auth0)) {
+    throw new Error('Auth0 must be loaded.');
+  }
+
+  angular.module('auth0.auth0', []).provider('angularAuth0', angularAuth0);
+
+  function angularAuth0() {
     this.init = function(config) {
       if (!config) {
-        throw new Error('clientID and domain must be provided to auth0');
+        throw new Error('Client ID and Domain are required to initialize Auth0.js');
+      }
+      if (config._telemetryInfo) {
+        config._telemetryInfo.env = angular.extend({}, this.config._telemetryInfo.env, {
+          'angular-auth0': "3.0.6"
+        });
+      } else {
+        config._telemetryInfo = {
+          name: 'angular-auth0',
+          version: "3.0.6",
+          env: {
+            'auth0-js': Auth0.version.raw
+          }
+        }
       }
       this.config = config;
     };
 
-    this.$get = ["$rootScope", function($rootScope) {
+    this.$get = [
+      '$rootScope',
+      function($rootScope) {
+        var Auth0Js = new Auth0.WebAuth(this.config);
+        var webAuth = {};
+        var functions = [];
 
-      var Auth0Js = new Auth0(this.config);
-      var auth0 = {};
-      var functions = [];
-      for (var i in Auth0Js) {
-        if (angular.isFunction(Auth0Js[i])) {
-          functions.push(i);
+        for (var i in Auth0Js) {
+          if (angular.isFunction(Auth0Js[i])) {
+            functions.push(i);
+          }
+          if (angular.isObject(Auth0Js[i])) {
+            webAuth[i] = Auth0Js[i];
+          }
         }
-      }
 
-      function wrapArguments(parameters) {
-        var lastIndex = parameters.length - 1,
-          func = parameters[lastIndex];
-        if (angular.isFunction(func)) {
-          parameters[lastIndex] = function() {
-            var args = arguments;
-            $rootScope.$evalAsync(function() {
-              func.apply(Auth0Js, args);
-            });
-          };
+        function wrapArguments(parameters) {
+          var lastIndex = parameters.length - 1,
+            func = parameters[lastIndex];
+          if (angular.isFunction(func)) {
+            parameters[lastIndex] = function() {
+              var args = arguments;
+              $rootScope.$evalAsync(function() {
+                func.apply(Auth0Js, args);
+              });
+            };
+          }
+          return parameters;
         }
-        return parameters;
-      }
 
-      for (var i = 0; i < functions.length; i++) {
-        auth0[functions[i]] = (function(name) {
-          var customFunction = function() {
-            return Auth0Js[name].apply(Auth0Js, wrapArguments(arguments));
-          };
-          return customFunction;
-        })(functions[i]);
+        for (var i = 0; i < functions.length; i++) {
+          webAuth[functions[i]] = (function(name) {
+            var customFunction = function() {
+              return Auth0Js[name].apply(Auth0Js, wrapArguments(arguments));
+            };
+            return customFunction;
+          })(functions[i]);
+        }
+
+        return webAuth;
       }
-      return auth0;
-    }];
+    ];
   }
 });
