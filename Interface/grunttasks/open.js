4cab9b898e8235a8537e7f7c50121110ef13bba8
Bo Henriksen <stumpdenmark@hotmail.com>
2020-05-18 19:52:33 +0200

    Update open.js

--- Interface/grunttasks/open.js
+++ Interface/grunttasks/open.js
@@ -1,6 +1,6 @@
 module.exports = {
     prototype: {
-        path: 'http://localhost:1508'
+        path: 'http://localhost:1508/#/task/1/page/108250'
     },
     sdk: {
         path: 'http://localhost:1510'
