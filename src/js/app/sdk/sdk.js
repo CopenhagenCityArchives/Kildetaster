4f2544b44aebe8084940025318b79d2cbc4a7bdc
jfeo <jens@feodor.dk>
2020-05-29 00:32:54 +0200

    Update sdk.js: Use import instead of define

--- src/js/app/sdk/sdk.js
+++ src/js/app/sdk/sdk.js
@@ -1,114 +1,69 @@
-define([
+import angular from 'angular';
+import angularBootstrap from 'angular-ui-bootstrap';
+import ngStorage from 'ngstorage';
+import angularGoogleAnalytics from 'angular-google-analytics';
 
-    'angular',
-    'angular-ui-bootstrap',
+import sdkRun from './sdk.run';
+import analyticsBootstrap from '../shared/analyticsBootstrap';
 
-    'ngstorage',
-    'angular-google-analytics',
+import searchApp from './search/search';
 
-    './sdk.run',
-    '../shared/analyticsBootstrap',
+import errorsController from './controllers/errors.controller';
+import useractivitiesController from './controllers/useractivities.controller';
+import fritekstSearchController from './controllers/fritekst-search.controller';
 
-    './search/search',
+import tokenService from '../shared/services/token.service';
+import tokenFactory from '../shared/services/token.factory';
 
-    './controllers/errors.controller',
-    './controllers/useractivities.controller',
-    './controllers/fritekst-search.controller',
+import pageService from '../shared/services/page.service';
+import taskService from '../shared/services/task.service';
+import errorService from '../shared/services/error.service';
+import userService from '../shared/services/user.service';
 
-    '../shared/services/token.service',
-    '../shared/services/token.factory',
+import userStatisticsDirective from './directives/user-statistics.directive';
+import taskunitsListDirective from './directives/taskunits-list.directive';
+import taskStatusDirective from './directives/task-status.directive';
+import taskProgressPlotDirective from './directives/task-progress-plot.directive';
+import progressbarDirective from './directives/progressbar.directive';
+import userDirective from '../shared/directives/user.directive';
+import shareLinkDirective from '../shared/directives/shareLink.directive';
+import constants from '../../../../constants.json';
 
-    '../shared/services/page.service',
-    '../shared/services/task.service',
-    '../shared/services/error.service',
-    '../shared/services/user.service',
+var sdkApp = angular.module('sdk', [
+    'ui.bootstrap',
+    'search',
+    'constants',
+    'ngStorage',
+    'angular-google-analytics'
+]);
 
-    './directives/user-statistics.directive',
-    './directives/taskunits-list.directive',
-    './directives/task-status.directive',
-    './directives/task-progress-plot.directive',
-    './directives/progressbar.directive',
-    '../shared/directives/user.directive',
-    '../shared/directives/shareLink.directive',
-    '../../../../constants.json'
+sdkApp.run(sdkRun);
+sdkApp.run(analyticsBootstrap.run);
 
+sdkApp.config(analyticsBootstrap.config);
 
-], function(
+sdkApp.controller('errorsController', errorsController);
+sdkApp.controller('useractivitiesController', useractivitiesController);
+sdkApp.controller('fritekstSearchController', fritekstSearchController);
 
-    ang,
-    angularBootstrap,
+sdkApp.service('tokenService', tokenService);
+sdkApp.factory('tokenFactory', tokenFactory);
 
-    ngStorage,
-    angularGoogleAnalytics,
+sdkApp.service('pageService', pageService);
+sdkApp.service('taskService', taskService);
+sdkApp.service('errorService', errorService);
+sdkApp.service('userService', userService);
 
-    sdkRun,
-    analyticsBootstrap,
+sdkApp.directive('userStatistics', userStatisticsDirective);
+sdkApp.directive('taskunitsList', taskunitsListDirective);
+sdkApp.directive('taskStatus', taskStatusDirective);
+sdkApp.directive('taskProgressPlot', taskProgressPlotDirective);
+sdkApp.directive('progressBar', progressbarDirective);
+sdkApp.directive('user', userDirective);
+sdkApp.directive('shareLink', shareLinkDirective);
 
-    searchApp,
-
-    errorsController,
-    useractivitiesController,
-    fritekstSearchController,
-
-    tokenService,
-    tokenFactory,
-
-    pageService,
-    taskService,
-    errorService,
-    userService,
-
-    userStatisticsDirective,
-    taskunitsListDirective,
-    taskStatusDirective,
-    taskProgressPlotDirective,
-    progressbarDirective,
-    userDirective,
-    shareLinkDirective,
-
-    constants
-) {
-
-    var sdkApp = angular.module('sdk', [
-        'ui.bootstrap',
-        'search',
-        'constants',
-        'ngStorage',
-        'angular-google-analytics'
-    ]);
-
-    sdkApp.run(sdkRun);
-    sdkApp.run(analyticsBootstrap.run);
-    
-    sdkApp.config(analyticsBootstrap.config);
-
-    sdkApp.controller('errorsController', errorsController);
-    sdkApp.controller('useractivitiesController', useractivitiesController);
-    sdkApp.controller('fritekstSearchController', fritekstSearchController);
-
-    //sdkApp.factory('accessTokenHttpInterceptor', tokenFactory);
-    sdkApp.service('tokenService', tokenService);
-    sdkApp.factory('tokenFactory', tokenFactory);
-
-    sdkApp.service('pageService', pageService);
-    sdkApp.service('taskService', taskService);
-    sdkApp.service('errorService', errorService);
-    sdkApp.service('userService', userService);
-
-    sdkApp.directive('userStatistics', userStatisticsDirective);
-    sdkApp.directive('taskunitsList', taskunitsListDirective);
-    sdkApp.directive('taskStatus', taskStatusDirective);
-    sdkApp.directive('taskProgressPlot', taskProgressPlotDirective);
-    sdkApp.directive('progressBar', progressbarDirective);
-    sdkApp.directive('user', userDirective);
-    sdkApp.directive('shareLink', shareLinkDirective);
-
-
-
-    angular.element(document).ready(function() {
-        angular.bootstrap(angular.element('[data-sdk-app]'), ['sdk']);
-    });
-
-    return sdkApp;
-
+angular.element(document).ready(function() {
+    angular.bootstrap(angular.element('[data-sdk-app]'), ['sdk']);
 });
+
+export default sdkApp;
