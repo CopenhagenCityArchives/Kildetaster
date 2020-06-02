f90a1bf6834a94ecb6d2348be5aef9fc1547e95d
Bo Henriksen <stumpdenmark@hotmail.com>
2020-06-02 13:29:09 +0200

    Add Express server developing with no hash urls

--- Interface/grunttasks/express.js
+++ Interface/grunttasks/express.js
@@ -1,20 +1,24 @@
-module.exports = {
+options = {
 	
 	options: {
 		hostname: '0.0.0.0',
-		livereload: true,
+		livereload: true
 	},
 
 	prototype: {
 		options: {
 			port: 1508,
-			bases: ['<%= package.prototype %>']
+			bases: ['<%= package.prototype %>'],
+			server: 'Interface/configs/express-server-prototype'
 		}
 	},
 	sdk: {
 		options: {
 			port: 1510,
-			bases: ['<%= package.sdk %>']
+			bases: ['<%= package.sdk %>'],
+			server: 'Interface/configs/express-server-sdk'
 		}
 	}
 };
+
+module.exports = options;
