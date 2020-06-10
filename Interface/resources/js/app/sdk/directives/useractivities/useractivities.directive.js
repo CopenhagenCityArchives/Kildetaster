06e3e54dacbb3e9754d54806bf7745be22148534
jfeo <jens@feodor.dk>
2020-06-10 15:34:04 +0200

    Update useractivities: New markup, display error state

--- Interface/resources/js/app/sdk/directives/useractivities/useractivities.directive.js
+++ Interface/resources/js/app/sdk/directives/useractivities/useractivities.directive.js
@@ -10,36 +10,30 @@ define([
             templateUrl: 'sdk/directives/useractivities/useractivities.directive.tpl.html',
 
             controller: ['$scope', function ($scope) {
+                $scope.loading = true;
+                $scope.error = false;
 
-                $scope.loading = false;
                 $scope.activities = [];
                 $scope.tasks = [];
         
-                $scope.userId = null;
-        
                 $scope.EDITORURL = EDITOR_URL;
-        
-                $scope.init = function() {
-        
-                    $scope.loading = true;
-        
-                    taskService.getTasks().then(function (response) {
-                        response.forEach(function (task) {
-                            $scope.tasks[task.id] = task;
-                        });
-                        userService.getUserActivities().then(function (response) {
-                            $scope.activities = response.filter(function (activity) {
-                                return activity.task_unit_pages_done < activity.unit_pages;
-                            });
-                        }).finally(function () {
-                            $scope.loading = false;
-                        });
+    
+                taskService.getTasks().then(function (response) {
+                    response.forEach(function (task) {
+                        $scope.tasks[task.id] = task;
                     });
-                };
-        
-                //Initialize logic
-                $scope.init();
-        
+                    userService.getUserActivities().then(function (response) {
+                        $scope.activities = response.filter(function (activity) {
+                            return activity.task_unit_pages_done < activity.unit_pages;
+                        });
+                    })
+                    .catch(function() {
+                        $scope.error = true;
+                    })
+                    .finally(function () {
+                        $scope.loading = false;
+                    });
+                });
             }]
         }
     }
