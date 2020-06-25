5c497cd8d2a83b8651339bc6f54c17c720b69094
Jonsabban <Sanox3@hotmail.dk>
2020-06-25 11:17:58 +0200

    editorurl + gallery styling

--- src/js/app/sdk/directives/gallery.directive.js
+++ src/js/app/sdk/directives/gallery.directive.js
@@ -1,14 +1,24 @@
-export default galleryDirective;
-function galleryDirective() {
-    return {
-        restrict: 'E',
+define([
 
-        scope: {
-            images: '=',
-            data: '='
-        },
+], function() {
+        
+    var galleryDirective = ['EDITOR_URL', function galleryDirective(EDITOR_URL) {
+        return {
+            restrict: 'E',
 
-        template: require('./gallery.directive.tpl.html'),        
-    }
+            scope: {
+                images: '=',
+                data: '=',
+                //editorUrl:  '='
+            },
 
-}
+            template: require('./gallery.directive.tpl.html'),
+            
+            link: function(scope, element, attr) {
+                scope.editorUrl = EDITOR_URL + '#/task/' + scope.data.task_id + '/page/' + scope.data.page_id + '/post/' + scope.data.post_id;
+            }
+        }
+    }];
+
+    return galleryDirective;
+});
