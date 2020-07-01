fde60f61cf524c7d9a0610c3103cd00395df29b6
jfeo <jens@feodor.dk>
2020-07-01 13:54:10 +0200

    Update search.run.js
    Configure $anchorScroll to scroll items out from under the top menu

--- src/js/app/sdk/search/search.run.js
+++ src/js/app/sdk/search/search.run.js
@@ -1,13 +1,9 @@
 define([
+    'jquery'
+], function($) {
 
-], function() {
-
-    var searchRun = ['$trace', function searchRun($trace) {
-
-        // Enable this to get debug info about transitions in the console
-        // https://ui-router.github.io/ng1/docs/1.0.0/enums/trace.category.html
-        //$trace.enable('TRANSITION');
-
+    var searchRun = ['$anchorScroll', function searchRun($anchorScroll) {
+        $anchorScroll.yOffset = $('header.top-menu'); // fixed position header element
     }];
 
     return searchRun;
