be12882575ce842fca2ec63ce186ef651d51c7b1
Jonsabban <Sanox3@hotmail.dk>
2020-07-10 10:53:55 +0200

    Report-error Directive: add_metadata variable

--- src/js/app/sdk/directives/report-error.directive.js
+++ src/js/app/sdk/directives/report-error.directive.js
@@ -13,28 +13,20 @@ define([
             template: require('./report-error.directive.tpl.html'),
 
             controller: ['$scope', 'errorService', function($scope, errorService) {
-
-                var that = this;
                 
                 this.selected = null;
                 this.selectedComment = null;
-
-                console.log(this);
-
-                console.log($scope);
 
                 // Lookup in the config to find the matching confic, based on collection_id
                
                 this.$onInit = function() {
                     errorService.getConfig().then(function (data) {
                         $scope.configData = data[$scope.data.collection_id].error_reports;
+                        $scope.add_metadata = data[$scope.data.collection_id].add_metadata;
                     })
                 }                
         
                 $scope.saveErrorReport = function saveErrorReport() {
-                    console.log("hit");
-                    console.log(that);
-                    console.log($scope);
 
                     var reportData = {
                         id: $scope.data.id,
@@ -46,14 +38,11 @@ define([
                         entity: $scope.selected.entity,
                         field: $scope.selected.field,
                         comment: $scope.selectedComment,
-                        add_metadata: $scope.configData.add_metadata,
+                        add_metadata: $scope.add_metadata,
                         
                         // used for police and erindring type posts
                         collection_id: $scope.data.collection_id
                     };
-
-                    console.log(reportData);
-                    console.log(errorService);
 
                     // Save the error report
                     errorService.createErrorReport(reportData).then(function(response) {
