4545a28870e45864b9f26a78fc59de8e0152b774
jfeo <jens@feodor.dk>
2020-08-04 15:20:06 +0200

    Rewrite adding access tokens for API, much of token.service

--- src/js/app/shared/services/token.factory.js
+++ src/js/app/shared/services/token.factory.js
@@ -1,47 +1,31 @@
+const { $q } = require("angular-ui-router");
+
 define([
 
 ], function () {
 
     //Create a http interceptor factory
-    var accessTokenHttpInterceptor = ['MAIN_DOMAIN', 'SOLR_API_URL', '$sessionStorage', function accessTokenHttpInterceptor(MAIN_DOMAIN, SOLR_API_URL, $sessionStorage) {
+    var accessTokenHttpInterceptor = ['$q', 'API_URL', 'tokenService', function accessTokenHttpInterceptor($q, API_URL, tokenService) {
 
         return {
 
             //For each request the interceptor will set the bearer token header.
             request: function ($config) {
-
-
-                if ($config.url !== MAIN_DOMAIN + '/index.php' && $config.url.indexOf(SOLR_API_URL) == -1) {
-
-                    if ($sessionStorage.tokenData) {
-                        //Fetch token from cookie
-                        var token = $sessionStorage.tokenData.access_token;
-
-                        //set authorization header
+                if ($config.url.startsWith(API_URL)) {
+                    return tokenService.getToken()
+                    .then(function(token) {
                         $config.headers['Authorization'] = 'Bearer ' + token;
-                    }
-
+                        return $q.resolve($config);
+                    })
+                    .catch(function() {
+                        return $q.resolve($config);
+                    });
                 } else {
-                    console.log("request sent to MAINURL or Solr, skipping authorization header");
+                    return $config;
                 }
-
-                return $config;
-
             },
 
             response: function (response) {
-                //if you get a token back in your response you can use
-                //the response interceptor to update the token in the
-                //stored in the cookie
-
-                // if (response.config.headers.yourTokenProperty) {
-                //       //fetch token
-                //       var token = response.config.headers.yourTokenProperty;
-
-                //       //set token
-                //       $cookies.put('token', token);
-                // }
-
                 return response;
             }
         };
--- src/js/app/shared/services/token.service.js
+++ src/js/app/shared/services/token.service.js
@@ -1,28 +1,20 @@
-import createAuth0Client from '@auth0/auth0-spa-js';
+import { Auth0Client } from '@auth0/auth0-spa-js';
 
 var tokenService = ['$q', '$sessionStorage', '$location', 'callbackService', 'AUTH0_CLIENTID', 'AUTH0_DOMAIN', function tokenService($q, $sessionStorage, $location, callbackService, AUTH0_CLIENTID, AUTH0_DOMAIN) {
 
+    const auth0 = new Auth0Client({
+        client_id: AUTH0_CLIENTID,
+        domain: AUTH0_DOMAIN,
+        redirect_uri: callbackService.getCallbackUrl()
+    });
+    
     return {
         login() {
-            return createAuth0Client({
-                client_id: AUTH0_CLIENTID,
-                domain: AUTH0_DOMAIN,
-                redirect_uri: callbackService.getCallbackUrl()
-            })
-            .then(function(auth0) {
-                return auth0.loginWithPopup();
-            })
+            return auth0.loginWithPopup();
         },
 
         getUser() { 
-            return createAuth0Client({
-                client_id: AUTH0_CLIENTID,
-                domain: AUTH0_DOMAIN,
-                redirect_uri: callbackService.getCallbackUrl()
-            })
-            .then(function(auth0) {
-                return auth0.getUser();
-            })
+            return auth0.getUser()
             .then(function(user) {
                 if (user === undefined) {
                     return $q.reject("Not logged in");
@@ -32,98 +24,11 @@ var tokenService = ['$q', '$sessionStorage', '$location', 'callbackService', 'AU
             })
         },
 
-        /**
-         * Check if the user is logged in, and return the access token if they are
-         */
-        getToken: function(allowEmptyResponse) {
-            var deferred = $q.defer();
-            allowEmptyResponse = allowEmptyResponse || false;
-
-            this.getStoredToken()
-            .then(function(tokenData) {
-                deferred.resolve(tokenData);
-            })
+        getToken() {
+            return auth0.getTokenSilently()
             .catch(function() {
-                if (allowEmptyResponse) {
-                    deferred.resolve({ user: null });
-                } else {
-                    // TODO: This should ensures that two parallel
-                    // authorizations do not interfere. Find a better solution.
-                    if (!$location.search().auth_access_token) {
-                        auth0.loginWithRedirect({
-                            audience: 'https://www.kbhkilder.dk/api',
-                            response_type: 'token',
-                            redirect_uri: callbackService.getCallbackUrl()
-                        });
-                    }
-                }
-            })
-
-            return deferred.promise;
-        },
-
-        getStoredToken: function() {
-            var deferred = $q.defer();
-
-            if ($sessionStorage.tokenData) {
-                deferred.resolve($sessionStorage.tokenData);
-            } else {
-                deferred.reject('No token data stored.');
-            }
-
-            return deferred.promise;
-        },
-
-        getUrlToken: function() {
-            var deferred = $q.defer();
-            var search = $location.search();
-
-            var hash = Object.keys(search)
-            .filter(function(key) { 
-                return key.startsWith('auth_');
-            })
-            .map(function(key) {
-                return key.replace(/^auth_/, '') + "=" + encodeURIComponent(search[key]);
-            })
-            .join('&');
-
-            console.log(hash, search);
-
-            angularAuth0.parseHash({ hash: hash }, function(err, authResult) {
-                if (err || !authResult) {
-                    console.log('Could not parse hash: ',err, hash);                        
-                    deferred.reject(err);
-                    return;
-                }
-            
-                angularAuth0.client.userInfo(authResult.accessToken, function(err, user) {
-                    if (err) {
-                        console.log('Error: Could not get user info from Auth0: ',err);
-                        deferred.reject(err);
-                        return;
-                    }
-                    //TODO: Hardcoded user id (Signe's user)
-                    user.userId = 601;
-
-                    tokenData =  {
-                        user: user,
-                        access_token: authResult.accessToken
-                    };
-
-                    $sessionStorage.tokenData = tokenData;
-
-                    // clean authorization search parameters
-                    Object.keys(search).forEach(function(key) {
-                        if (key.startsWith('auth_')) {
-                            $location.search(key, null);
-                        }
-                    })
-
-                    deferred.resolve(tokenData)
-                });
+                return auth0.getTokenWithPopup();
             });
-
-            return deferred.promise;
         }
     };
 
