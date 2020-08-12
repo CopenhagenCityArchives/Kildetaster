bec5e5129087a74d014116b9107a01526e8e3bb9
jfeo <jens@feodor.dk>
2020-08-12 16:00:21 +0200

    Update task-status.directive: Clear loading indicators

--- src/js/app/sdk/directives/task-status.directive.js
+++ src/js/app/sdk/directives/task-status.directive.js
@@ -1,58 +1,58 @@
-define([], function () {
+export default ['taskService', '$timeout', function(taskService, $timeout) {
+    // Counter for how many times the directive has been used, used to build unique id's
+    var num = 0;
+    return {
+        restrict: 'E',
 
-    var taskStatusDirective =  ['taskService', function(taskService) {
+        template: require('./task-status.directive.tpl.html'),
 
-        // Counter for how many times the directive has been used, used to build unique id's
-        var num = 0;
-        return {
-            restrict: 'E',
+        scope: {
+            'taskId': '='
+        },
 
-            template: require('./task-status.directive.tpl.html'),
+        link: function (scope, element, attr) {
+            scope.num = num;
+            scope.unitsCount = 0;
+            scope.activeUnitsCount = 0;
+            scope.unitsDoneCount = 0;
+            scope.unitsDoingCount = 0;
+            scope.loading = true;
 
-            scope: {
-                'taskId': '='
-            },
-
-            link: function (scope, element, attr) {
-                scope.num = num;
-                scope.unitsCount = 0;
-                scope.activeUnitsCount = 0;
-                scope.unitsDoneCount = 0;
-                scope.unitsDoingCount = 0;
-
-                taskService.getTask(scope.taskId)
-                    .then(function (response) {
-                        scope.task = response;
-                    });
-
-                taskService.getUnits({
-                    task_id: scope.taskId
-                }).then(function (response) {
-                    // open/active units count
-                    scope.activeUnitsCount = response.length;
-
-                    // compute done and doing counts
-                    response.forEach(function(unit) {
-                        if (unit.pages_done == unit.pages)
-                            scope.unitsDoneCount++;
-                        else if (unit.pages_done > 0)
-                            scope.unitsDoingCount++;
-                    });
-
-                    // also get inactive to compute total count
-                    taskService.getUnits({
-                        task_id: scope.taskId,
-                        index_active: 0
-                    }).then(function (inactiveResponse) {
-                        scope.unitsCount = response.length + inactiveResponse.length;
-                    });
+            taskService.getTask(scope.taskId)
+                .then(function (response) {
+                    scope.task = response;
                 });
-                // Increment the counter for other instances of the directive
-                num++;
-            }
-        };
-    }];
 
-    return taskStatusDirective;
+            taskService.getUnits({
+                task_id: scope.taskId
+            })
+            .then(function (response) {
+                // open/active units count
+                scope.activeUnitsCount = response.length;
 
-});
+                // compute done and doing counts
+                response.forEach(function(unit) {
+                    if (unit.pages_done == unit.pages)
+                        scope.unitsDoneCount++;
+                    else if (unit.pages_done > 0)
+                        scope.unitsDoingCount++;
+                });
+
+                // also get inactive to compute total count
+                return taskService.getUnits({
+                    task_id: scope.taskId,
+                    index_active: 0
+                })
+            })
+            .then(function (inactiveResponse) {
+                scope.unitsCount = response.length + inactiveResponse.length;
+            })
+            .finally(function() {
+                scope.loading = false;
+            });
+            
+            // Increment the counter for other instances of the directive
+            num++;
+        }
+    };
+}];
