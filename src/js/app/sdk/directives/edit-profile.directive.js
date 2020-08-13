378e37d5c6dee0b99d5382dbe44b41ab5913b62e
jfeo <jens@feodor.dk>
2020-08-13 11:12:34 +0200

    Update edit-profile.directive: Update password

--- src/js/app/sdk/directives/edit-profile.directive.js
+++ src/js/app/sdk/directives/edit-profile.directive.js
@@ -2,6 +2,43 @@ export default [function () {
     return {
         restrict: 'E',
         scope: {},
-        template: require('./edit-profile.directive.tpl.html')
+        template: require('./edit-profile.directive.tpl.html'),
+        controller: ['$scope', '$element', 'userService', function($scope, $element, userService) {
+            $scope.editPassword = false;
+            $scope.passwordValue = undefined;
+            $scope.passwordRepeatValue = undefined;
+            $scope.passwordChanged = false;
+            $scope.passwordError = false;
+            $scope.passwordUpdating = false;
+            
+            $scope.submitPassword = function(event) {
+                this.passwordChanged = false;
+                this.passwordError = false;
+                this.passwordUpdating = true;
+
+                if (event.target.checkValidity()) {
+                    userService.updateUserProfile({'password': this.passwordValue})
+                    .then(function() {
+                        $scope.passwordChanged = true;
+                    })
+                    .catch(function(err) {
+                        $scope.passwordError = true;
+                    })
+                    .finally(function() {
+                        $scope.editPassword = false;
+                        $scope.passwordUpdating = false;
+                    })
+                }
+            }
+
+            $scope.checkRepeat = function() {
+                var elem = $element.find('#edit-profile-input-password-2')[0];
+                if (this.passwordRepeatValue && this.passwordValue == this.passwordRepeatValue) {
+                    elem.setCustomValidity("");
+                } else {
+                    elem.setCustomValidity("De to kodeord skal være ens.");
+                }
+            }
+        }]
     }
 }]
