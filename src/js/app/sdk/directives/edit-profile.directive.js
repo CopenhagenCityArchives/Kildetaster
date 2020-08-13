f00c8b6ad02764538f03037f8f1c4f2b2e0fd286
jfeo <jens@feodor.dk>
2020-08-13 17:37:50 +0200

    Update edit-profile.directive.js: Simpler design

--- src/js/app/sdk/directives/edit-profile.directive.js
+++ src/js/app/sdk/directives/edit-profile.directive.js
@@ -3,61 +3,114 @@ export default [function () {
         restrict: 'E',
         scope: {},
         template: require('./edit-profile.directive.tpl.html'),
-        controller: ['$scope', '$element', 'userService', function($scope, $element, userService) {
-            $scope.editPassword = false;
-            $scope.passwordValue = undefined;
-            $scope.passwordRepeatValue = undefined;
-            $scope.passwordChanged = false;
-            $scope.passwordError = false;
-            $scope.passwordUpdating = false;
+        controller: ['$scope', '$element', '$timeout', 'authService', 'userService', function($scope, $element, $timeout, authService, userService) {
+            $scope.user = null;
 
-            $scope.emailSuccess = function() {
-                return 'Din e-mail er opdateret!';
+            $scope.loading = true;
+            $scope.saving = false;
+            $scope.editing = false;
+            $scope.errorSaving = null;
+            $scope.successSaving = null;
+
+            $scope.nickname = null;
+            $scope.email = null;
+            $scope.emailRepeat = null;
+            $scope.password = null;
+            $scope.passwordRepeat = null;
+
+            $scope.edit = function() {
+                $scope.editing = true;
+
+                $scope.nickname = $scope.user.nickname;
+                $scope.email = $scope.user.email;
+                $scope.emailRepeat = $scope.user.email;
+                $scope.errorSaving = "";
+                $scope.successSaving = "";
             }
-            $scope.emailError = function() {
-                return 'Din e-mail blev ikke opdateret.';
+
+            $scope.cancel = function() {
+                $scope.editing = false;
+                $scope.errorSaving = "";
+                $scope.successSaving = "";
             }
-            $scope.nicknameSuccess = function() {
-                return 'Dit brugernavn blev opdateret!';
+
+            $scope.submit = function() {
+                if (!$element.find('form')[0].checkValidity()) {
+                    return;
+                }
+
+                var profile = {};
+                if (this.nickname && this.nickname != this.user.nickname) {
+                    profile.nickname = this.nickname;
+                }
+                if (this.password) {
+                    profile.password = this.password;
+                }
+                if (this.email && this.email != this.user.email) {
+                    profile.email = this.email;
+                }
+                if (!profile.nickname && !profile.password && !profile.email) {
+                    return;
+                }
+
+
+                $scope.errorSaving = "";
+                $scope.successSaving = "";
+                this.saving = true;
+
+                userService.updateUserProfile(profile)
+                .then(function(result) {
+                    $scope.user = result.data;
+                    $scope.successSaving = "Brugeroplysningerne blev gemt!";
+                    $scope.password = null;
+                    $scope.passwordRepeat = null;
+                })
+                .catch(function(err) {
+                    if (err.status == 400 && err.data && err.data.message == 'Username already exists') {
+                        $scope.errorSaving = "Brugeroplysninger blev ikke gemt. Brugernavnet er optaget."
+                    } else {
+                        $scope.errorSaving = "Brugeroplysninger blev ikke gemt."
+                    }
+                })
+                .finally(function() {
+                    $scope.$apply(function() {
+                        $scope.editing = false;
+                        $scope.saving = false;
+                    });
+                })
             }
-            $scope.nicknameError = function(err) {
-                if (err.status == 400 && err.data && err.data.message == 'Username already exists') {
-                    return 'Brugernavnet er optaget.';
+
+            $scope.checkEmailRepeat = function() {
+                if (this.email != this.emailRepeat) {
+                    $element.find('#edit-profile-email')[0].setCustomValidity('De to e-mailadresser skal være ens.');
+                    $element.find('#edit-profile-email-repeat')[0].setCustomValidity('De to e-mailadresser skal være ens.');
                 } else {
-                    return 'Dit brugernavn blev ikke opdateret.';
+                    $element.find('#edit-profile-email')[0].setCustomValidity('');
+                    $element.find('#edit-profile-email-repeat')[0].setCustomValidity('');
+                }
+            }
+
+            $scope.checkPasswordRepeat = function() {
+                if ((this.password || this.passwordRepeat) && this.password != this.passwordRepeat) {
+                    $element.find('#edit-profile-password')[0].setCustomValidity('De to kodeord skal være ens.');
+                    $element.find('#edit-profile-password-repeat')[0].setCustomValidity('De to kodeord skal være ens.');
+                } else {
+                    $element.find('#edit-profile-password')[0].setCustomValidity('');
+                    $element.find('#edit-profile-password-repeat')[0].setCustomValidity('');
                 }
             }
             
-            $scope.submitPassword = function(event) {
-                $scope.passwordChanged = false;
-                $scope.passwordError = false;
-                $scope.passwordUpdating = true;
-
-                if (event.target.checkValidity()) {
-                    userService.updateUserProfile({'password': this.passwordValue})
-                    .then(function() {
-                        $scope.passwordChanged = true;
-                    })
-                    .catch(function(err) {
-                        $scope.passwordError = true;
-                    })
-                    .finally(function() {
-                        $scope.passwordUpdating = false;
-                    })
-                }
-            }
-
-            $scope.checkRepeat = function() {
-                var elem1 = $element.find('#edit-profile-input-password-1')[0];
-                var elem2 = $element.find('#edit-profile-input-password-2')[0];
-                if (this.passwordRepeatValue && this.passwordValue == this.passwordRepeatValue) {
-                    elem1.setCustomValidity("");
-                    elem2.setCustomValidity("");
-                } else {
-                    elem1.setCustomValidity("De to kodeord skal være ens.");
-                    elem2.setCustomValidity("De to kodeord skal være ens.");
-                }
-            }
+            
+            authService.getUser()
+            .then(function (user) {
+                $scope.user = user;
+            })
+            .catch(function () {
+                $scope.error = true;
+            })
+            .finally(function () {
+                $scope.loading = false;
+            })
         }]
     }
 }]
