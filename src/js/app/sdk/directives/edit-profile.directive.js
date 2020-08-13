d5e72a4e613242476f93475ebd65090a98cd2145
jfeo <jens@feodor.dk>
2020-08-13 14:50:43 +0200

    Update edit-profile.directive
    Do hide password fields after success change

--- src/js/app/sdk/directives/edit-profile.directive.js
+++ src/js/app/sdk/directives/edit-profile.directive.js
@@ -42,7 +42,6 @@ export default [function () {
                         $scope.passwordError = true;
                     })
                     .finally(function() {
-                        $scope.editPassword = false;
                         $scope.passwordUpdating = false;
                     })
                 }
