eb50c07efb485d60d904dfc1018f9ee2d2770b3c
jfeo <jens@feodor.dk>
2020-09-07 10:15:35 +0200

    Update callback.service: Optionally use callback page

--- src/js/app/shared/services/callback.service.js
+++ src/js/app/shared/services/callback.service.js
@@ -1,13 +1,18 @@
 var callbackService = ['$location', 'CALLBACK_URL', function($location, CALLBACK_URL) {
     return {
-        getCallbackUrl() {
+        getCallbackUrl(useCallbackPage) {
             var url = $location.absUrl();
-            return url;
-
-            if (url.indexOf('?') != -1) {
-                url = url.replace('?', '?AUTH_PARAMS&')
+            
+            if (useCallbackPage) {
+                if (url.indexOf('?') != -1) {
+                    url = url.replace('?', '?AUTH_PARAMS&')
+                } else {
+                    url = url + "?AUTH_PARAMS";
+                }
+                
+                CALLBACK_URL + "?url=" + encodeURIComponent(url);
             } else {
-                url = url + "?AUTH_PARAMS";
+                return url;
             }
 
             return CALLBACK_URL + '?url=' + encodeURIComponent(url);
